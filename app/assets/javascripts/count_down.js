075f3bf1e9e245efba03866f11989e71828181d0
wai-doi <yusuke.doi@esm.co.jp>
2019-04-28 19:19:06 +0900

    Add count down js

---
+++ app/assets/javascripts/count_down.js
@@ -0,0 +1,28 @@
+$(function() {
+  countDown();
+});
+function countDown() {
+  var startDateTime = new Date();
+  var endDateTime = new Date("April 01,2020 00:00:00");
+  var left = endDateTime - startDateTime;
+  var a_day = 24 * 60 * 60 * 1000;
+
+  // 期限から現在までの『残時間の日の部分』
+  var d = Math.floor(left / a_day) 
+
+  // 期限から現在までの『残時間の時間の部分』
+  var h = Math.floor((left % a_day) / (60 * 60 * 1000)) 
+
+  // 残時間を秒で割って残分数を出す。
+  // 残分数を60で割ることで、残時間の「時」の余りとして、『残時間の分の部分』を出す
+  var m = Math.floor((left % a_day) / (60 * 1000)) % 60 
+
+  // 残時間をミリ秒で割って、残秒数を出す。
+  // 残秒数を60で割った余りとして、「秒」の余りとしての残「ミリ秒」を出す。
+  // 更にそれを60で割った余りとして、「分」で割った余りとしての『残時間の秒の部分』を出す
+  var s = Math.floor((left % a_day) / 1000) % 60 % 60 
+
+  $("#TimeLeft").text(d + '日' + h + '時間' + m + '分' + s + '秒');
+  setTimeout('countDown()', 1000);
+}
+
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,6 @@
 class EventsController < ApplicationController
+  def index
+    @name = 'RubyKaigi 2020'
+    @date = DateTime.new(2020, 4, 9)
+  end
 end
